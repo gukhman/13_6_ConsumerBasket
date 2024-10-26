c6c6c33f9c47c4c2d4b89c79b589d21ec4cc276c
gukhman <gukhman@gmail.com>
2024-10-26 10:30:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a13_6_consumerbasket/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a13_6_consumerbasket
+
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupWindowInsets(R.id.main)
+        setupToolbar(R.id.toolbar, false)
+    }
+}
